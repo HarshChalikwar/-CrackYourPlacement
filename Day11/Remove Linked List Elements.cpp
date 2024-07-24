56ef7884f8312d9ad7f82af061c3d2d604855ad3
Harsh Chalikwar <f20212878@goa.bits-pilani.ac.in>
2024-07-24 23:25:24 +0530

    new question

--- Day11/Remove Linked List Elements.cpp
+++ Day11/Remove Linked List Elements.cpp
@@ -11,6 +11,23 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        
+        ListNode* prev = new ListNode(-1);
+        ListNode* head2 = prev;
+        ListNode* node = head;
+        while(node != nullptr)
+        {
+            if(node-> val == val)
+            {
+                prev-> next = node -> next;
+                node = node -> next;
+            }
+            else
+            {
+                prev -> next = node;
+                prev = prev -> next;
+                node = node->next;
+            }
+        }   
+        return head2->next;
     }
 };
